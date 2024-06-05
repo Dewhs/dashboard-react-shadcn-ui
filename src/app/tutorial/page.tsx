e73886f90e674c8e4fc9c38093d56c85a26469b5
Roger <chevreuil.chauve@gmail.com>
2024-06-05 18:15:08 +0200

    feat(responsive) add responsive for mobile

--- src/app/tutorial/page.tsx
+++ src/app/tutorial/page.tsx
@@ -5,7 +5,6 @@ const Tutorial = () => {
     return (
         <div>
             <h1>Tutorial</h1>
-            <p>This is a basic React component.</p>
         </div>
     );
 };
--- src/components/global/side-bar.tsx
+++ src/components/global/side-bar.tsx
@@ -3,15 +3,23 @@
 import React, { ReactNode, use, useEffect, useState } from "react";
 import { ModeToggle } from "./theme-button";
 import {
+  ArrowDown01,
+  ArrowDownIcon,
   ArrowLeftRight,
+  ArrowUp,
+  ChevronDown,
   CircleUser,
+  Crown,
   HelpCircle,
   HomeIcon,
+  Info,
   Instagram,
   LineChart,
+  Menu,
   Package2,
   PanelLeftClose,
   PanelLeftOpen,
+  Stars,
   Twitter,
   VideoIcon,
   Youtube,
@@ -32,6 +40,42 @@ import {
 
 import { ThemeProvider } from "@/components/global/theme-provider";
 import { Separator } from "@/components/ui/separator";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Progress } from "@/components/ui/progress";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+const navLinkClass =
+  "flex items-center gap-3 px-3 py-2 rounded-lg font-medium text-muted-foreground transition-all hover:text-primary focus:bg-muted focus:text-primary";
+
+const isActiveLinkClass = "bg-muted text-primary";
 
 const SideBar = ({
   children,
@@ -44,22 +88,15 @@ const SideBar = ({
   ) : (
     <PanelLeftClose className="size-5" />
   );
-
-  const pathname = usePathname();
-  const isActiveLinkClass = "bg-muted text-primary";
-  const navLinkClass =
-    "flex items-center gap-3 px-3 py-2 rounded-lg font-medium text-muted-foreground transition-all hover:text-primary focus:bg-muted focus:text-primary";
-
   return (
     <body className="h-screen">
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <div className="flex flex-col h-full w-screen">
+          {/* HEADER */}
+
           <header className="flex border-b justify-between items-center px-6 py-4">
-            <div className="flex gap-4 items-center">
-              <Link href="#" className="flex items-center gap-3 px-3">
-                <Package2 className="size-6" />
-                <h1 className="font-medium text-lg">Acme Inc</h1>
-              </Link>
+            <div className="max-md:hidden flex gap-4 items-center">
+              <CompanyLogoLink />
               <Button
                 variant="ghost"
                 size="icon"
@@ -72,91 +109,36 @@ const SideBar = ({
                 <span className="sr-only">Toggle sidebar</span>
               </Button>
             </div>
-            <div className="flex gap-4">
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <Menu className="size-6" />
+                </Button>
+              </SheetTrigger>
+              <SheetContent className="flex flex-col gap-6 w-[275px]" side={"left"}>
+                <SheetTitle>
+                  <CompanyLogoLink />
+                </SheetTitle>
+                <AsideContent isActive={false} />
+              </SheetContent>
+            </Sheet>
+            {/*<UpgradeHoverCard />*/}
+            <div className="flex gap-4 items-center">
               <ModeToggle />
               <AvatarButton />
             </div>
           </header>
           <div className="flex flex-grow h-full overflow-hidden">
             <aside
-              className={` flex flex-col justify-between py-2 border-r transition-all ease-linear duration-500 ${
+              className={`max-md:hidden flex flex-col justify-between py-2 border-r transition-all ease-linear duration-500 ${
                 isActive
                   ? "min-w-[0px] max-w-[100px]"
                   : "min-w-[150px] max-w-[500px]"
               }`}
             >
-              <nav className="grid items-start font-medium gap-2 px-4">
-                <p className="text-muted-foreground text-sm">Main</p>
-                <Link
-                  href="/"
-                  className={`${
-                    pathname === "/" ? isActiveLinkClass : ""
-                  } ${navLinkClass}`}
-                >
-                  <HomeIcon className="size-6" />
-                  <p className={`${isActive ? "hidden" : ""}`}>Home</p>
-                </Link>
-                <Link
-                  href="/analytics"
-                  className={`${
-                    pathname === "/analytics" ? isActiveLinkClass : ""
-                  } ${navLinkClass}`}
-                >
-                  <LineChart className="size-6" />
-                  <p className={`${isActive ? "hidden" : ""}`}>Analytics</p>
-                </Link>
-                <Link
-                  href="/transactions"
-                  className={`${
-                    pathname === "/transactions" ? isActiveLinkClass : ""
-                  } ${navLinkClass}`}
-                >
-                  <ArrowLeftRight className="size-6" />
-                  <p className={`${isActive ? "hidden" : ""}`}>Transactions</p>
-                </Link>
-
-                <p className="text-muted-foreground text-sm mt-4">More</p>
-                <Link
-                  href="/tutorial"
-                  className={`${
-                    pathname === "/tutorial" ? isActiveLinkClass : ""
-                  } ${navLinkClass}`}
-                >
-                  <VideoIcon className="size-6" />
-                  <p
-                    className={`${
-                      isActive ? "hidden opacity-0" : "opacity-100"
-                    } transition-all duration-500`}
-                  >
-                    Tutorial
-                  </p>
-                </Link>
-              </nav>
-
-              {/*   FOOTER   */}
-              <div className={`flex flex-col gap-2 justify-center`}>
-                <div
-                  className={`flex ${
-                    isActive ? "flex-col" : ""
-                  }  justify-center items-center gap-2`}
-                >
-                  <Link href="#" className={navLinkClass}>
-                    <Youtube className="size-5" />
-                  </Link>
-                  <Link href="#" className={navLinkClass}>
-                    <Instagram className="size-5" />
-                  </Link>
-
-                  <Link href="#" className={navLinkClass}>
-                    <Twitter className="size-5" />
-                  </Link>
-                </div>
-                <Separator />
-                <p className="flex justify-center text-sm truncate">
-                  {isActive ? "© 2024" : "© 2024 Acme Inc"}
-                </p>
-              </div>
+              <AsideContent isActive={isActive} />
             </aside>
+            {/* MAIN */}
             <div className="overflow-scroll flex-grow">{children}</div>
           </div>
         </div>
@@ -167,17 +149,234 @@ const SideBar = ({
 
 export default SideBar;
 
+const AsideContent = ({ isActive }: { isActive: boolean }) => {
+  return (
+    <>
+      {/*   NAVIGATION   */}
+      <AsideNav isActive={isActive} />
+
+      {!isActive && (
+        <Card className="max-w-[250px] md:mx-4 mt-auto mb-8">
+          <CardHeader>
+            <CardTitle className="flex gap-2">Upgrade to Pro</CardTitle>
+            <CardDescription>
+              Unlock all features and get unlimited access to our support team.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-4">
+            <Progress value={33} aria-label="Test" />
+            <p className="text-muted-foreground text-sm text-center">10 left</p>
+            <Button size="sm" className="w-full flex gap-2">
+              <Stars className="size-4" />
+              Upgrade
+            </Button>
+          </CardContent>
+        </Card>
+      )}
+
+      {/*   FOOTER   */}
+      <div className={`flex flex-col gap-2 justify-center`}>
+        <div
+          className={`flex ${
+            isActive ? "flex-col" : ""
+          }  justify-center items-center gap-2`}
+        >
+          <Link href="#" className={navLinkClass}>
+            <Youtube className="size-5" />
+          </Link>
+          <Link href="#" className={navLinkClass}>
+            <Instagram className="size-5" />
+          </Link>
+
+          <Link href="#" className={navLinkClass}>
+            <Twitter className="size-5" />
+          </Link>
+        </div>
+        <Separator className="max-md:hidden" />
+        <p className="flex justify-center text-sm truncate">
+          {isActive ? "© 2024" : "© 2024 Acme Inc"}
+        </p>
+      </div>
+    </>
+  );
+};
+
+const AsideNav = ({ isActive }: { isActive: boolean }) => {
+  const pathname = usePathname();
+  return (
+    <nav className="grid items-start font-medium gap-2 px-4">
+      <p className="text-muted-foreground text-sm">Main</p>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link
+              href="/"
+              className={`${
+                pathname === "/" ? isActiveLinkClass : ""
+              } ${navLinkClass}`}
+            >
+              <HomeIcon className="size-6" />
+              <p className={`${isActive ? "hidden" : ""}`}>Home</p>
+            </Link>
+          </TooltipTrigger>
+          {isActive && <TooltipContent side="right">Home</TooltipContent>}
+        </Tooltip>
+
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link
+              href="/analytics"
+              className={`${
+                pathname === "/analytics" ? isActiveLinkClass : ""
+              } ${navLinkClass}`}
+            >
+              <LineChart className="size-6" />
+              <p className={`${isActive ? "hidden" : ""}`}>Analytics</p>
+            </Link>
+          </TooltipTrigger>
+          {isActive && <TooltipContent side="right">Analytics</TooltipContent>}
+        </Tooltip>
+
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link
+              href="/transactions"
+              className={`${
+                pathname === "/transactions" ? isActiveLinkClass : ""
+              } ${navLinkClass}`}
+            >
+              <ArrowLeftRight className="size-6" />
+              <p className={`${isActive ? "hidden" : ""}`}>Transactions</p>
+            </Link>
+          </TooltipTrigger>
+          {isActive && (
+            <TooltipContent side="right">Transactions</TooltipContent>
+          )}
+        </Tooltip>
+
+        <p className="text-muted-foreground text-sm mt-4">More</p>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link
+              href="/tutorial"
+              className={`${
+                pathname === "/tutorial" ? isActiveLinkClass : ""
+              } ${navLinkClass}`}
+            >
+              <VideoIcon className="size-6" />
+              <p
+                className={`${
+                  isActive ? "hidden opacity-0" : "opacity-100"
+                } transition-all duration-500`}
+              >
+                Tutorial
+              </p>
+            </Link>
+          </TooltipTrigger>
+          {isActive && <TooltipContent side="right">Tutorial</TooltipContent>}
+        </Tooltip>
+      </TooltipProvider>
+    </nav>
+  );
+};
+
+const CompanyLogoLink = () => {
+  return (
+    <Link href="#" className="flex items-center gap-3 px-3">
+      <Package2 className="size-6" />
+      <h1 className="font-medium text-lg">Acme Inc</h1>
+    </Link>
+  );
+};
+
+function CardUpgrade() {
+  return (
+    <div className="mt-auto">
+      <Card>
+        <CardHeader>
+          <CardTitle>Upgrade to Pro</CardTitle>
+          <CardDescription>
+            Unlock all features and get unlimited access to our support team.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Button size="sm" className="w-full">
+            Upgrade
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+const UpgradeHoverCard = () => {
+  return (
+    <HoverCard>
+      <HoverCardTrigger asChild>
+        <Button
+          variant={"ghost"}
+          className="flex gap-4 items-center max-md:hidden"
+        >
+          <p className="text-muted-foreground text-sm text-nowrap">10 lefts</p>
+          <Progress value={33} className="min-w-60" />
+          <ChevronDown className="size-6" />
+        </Button>
+      </HoverCardTrigger>
+      <HoverCardContent className="w-full">
+        <UpgradeHoverCardContent />
+      </HoverCardContent>
+    </HoverCard>
+  );
+};
+
+const UpgradeHoverCardContent = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-between gap-6">
+        <div className="flex items-center gap-4">
+          <Crown className="size-6" />
+          <div className="flex flex-col">
+            <p className="text-sm font-medium">Subscribe to Pro</p>
+            <div className="text-muted-foreground text-xs">
+              Get unlimited features
+            </div>
+          </div>
+        </div>
+        <Button size="sm" className="flex gap-2">
+          <Stars className="size-4" />
+          Upgrade
+        </Button>
+      </div>
+    </div>
+  );
+};
+
 const AvatarButton = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="secondary" size="icon" className="rounded-full">
-          <CircleUser className="size-5" />
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="User" />
+            <AvatarFallback>U</AvatarFallback>
+          </Avatar>
           <span className="sr-only">Toggle user menu</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuLabel>
+          <div className="flex flex-col">
+            <div className="flex justify-between items-center">
+              John Doe
+              <Badge variant={"default"} className="text-xs bg-sky-500">
+                Free
+              </Badge>
+            </div>
+            <div className="text-muted-foreground text-sm font-normal">
+              john.doe@gmail.com
+            </div>
+          </div>
+        </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem>Settings</DropdownMenuItem>
         <DropdownMenuItem>Support</DropdownMenuItem>
